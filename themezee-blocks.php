a350518fd1bcd22f0ee53826a88c5fbcec275965
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-04-17 13:44:50 +0200

    added template functions

--- themezee-blocks.php
+++ themezee-blocks.php
@@ -99,6 +99,9 @@ class ThemeZee_Blocks {
 		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/admin/class-themezee-blocks-settings.php';
 		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/admin/class-themezee-blocks-settings-page.php';
 
+		// Include functions.
+		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/template-functions.php';
+
 		// Include Blocks for server-side.
 		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/blocks/class-themezee-blocks-magazine-grid.php';
 	}
