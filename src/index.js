ddf5768c0e503638e159b5ae43834c8b5b7cc969
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-08-06 11:46:02 +0200

    improved default styling of magazine posts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 /**
+ * Import Default Styling
+ */
+import './blocks/editor.scss';
+import './blocks/style.scss';
+
+/**
  * Import Blocks
  */
-import './blocks/style.scss';
 import './blocks/magazine-grid';
 import './blocks/magazine-horizontal';
 import './blocks/magazine-list';
