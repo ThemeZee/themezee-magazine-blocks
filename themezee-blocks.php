4b1e13ba6fda08488862bd4202a38de571791a89
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-08-08 15:14:35 +0200

    removed settings page and plugin updater

--- themezee-blocks.php
+++ themezee-blocks.php
@@ -26,7 +26,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @package ThemeZee Blocks
  */
 class ThemeZee_Blocks {
-
 	/**
 	 * Call all Functions to setup the Plugin
 	 *
@@ -36,7 +35,6 @@ class ThemeZee_Blocks {
 	 * @return void
 	 */
 	static function setup() {
-
 		// Setup Constants.
 		self::constants();
 
@@ -56,7 +54,6 @@ class ThemeZee_Blocks {
 	 * @return void
 	 */
 	static function constants() {
-
 		// Define Version Number.
 		define( 'THEMEZEE_BLOCKS_VERSION', '1.0' );
 
@@ -68,12 +65,6 @@ class ThemeZee_Blocks {
 
 		// Plugin Root File.
 		define( 'THEMEZEE_BLOCKS_PLUGIN_FILE', __FILE__ );
-
-		// Define Product ID.
-		define( 'THEMEZEE_BLOCKS_PRODUCT_ID', 338 );
-
-		// Define Update API URL.
-		define( 'THEMEZEE_BLOCKS_STORE_API_URL', 'https://themezee.com' );
 	}
 
 	/**
@@ -91,13 +82,6 @@ class ThemeZee_Blocks {
 	 * @return void
 	 */
 	static function includes() {
-		// Include Plugin Updater.
-		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/admin/class-themezee-blocks-plugin-updater.php';
-
-		// Include Plugin Settings.
-		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/admin/class-themezee-blocks-settings.php';
-		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/admin/class-themezee-blocks-settings-page.php';
-
 		// Include Magazine Cache Class.
 		require_once THEMEZEE_BLOCKS_PLUGIN_DIR . '/includes/class-themezee-blocks-magazine-cache.php';
 
@@ -118,18 +102,11 @@ class ThemeZee_Blocks {
 	 * @return void
 	 */
 	static function setup_actions() {
-
 		// Enqueue Block Assets.
 		add_action( 'init', array( __CLASS__, 'enqueue_block_assets' ) );
 
 		// Add block category.
 		add_filter( 'block_categories', array( __CLASS__, 'block_categories' ), 10, 2 );
-
-		// Add License Key admin notice.
-		add_action( 'admin_notices', array( __CLASS__, 'license_key_admin_notice' ) );
-
-		// Add plugin updater.
-		add_action( 'admin_init', array( __CLASS__, 'plugin_updater' ), 0 );
 	}
 
 	/**
@@ -138,7 +115,6 @@ class ThemeZee_Blocks {
 	 * @return void
 	 */
 	static function enqueue_block_assets() {
-
 		// Register block styles for both frontend + backend.
 		wp_register_style(
 			'themezee-blocks',
@@ -196,10 +172,8 @@ class ThemeZee_Blocks {
 	 * @return $script Data Dispatch code.
 	 */
 	static function get_dispatch_data() {
-		$script = '';
-
 		// Add Plugin URL.
-		$script .= sprintf( 'wp.data.dispatch( "themezee-blocks-store" ).setPluginURL( %s );', wp_json_encode( THEMEZEE_BLOCKS_PLUGIN_URL ) );
+		$script = sprintf( 'wp.data.dispatch( "themezee-blocks-store" ).setPluginURL( %s );', wp_json_encode( THEMEZEE_BLOCKS_PLUGIN_URL ) );
 
 		return $script;
 	}
@@ -219,64 +193,6 @@ class ThemeZee_Blocks {
 				),
 			)
 		);
-	}
-
-	/**
-	 * Add license key admin notice
-	 *
-	 * @return void
-	 */
-	static function license_key_admin_notice() {
-		global $pagenow;
-
-		// Display only on Plugins and Updates page.
-		if ( ! ( 'plugins.php' == $pagenow or 'update-core.php' == $pagenow ) ) {
-			return;
-		}
-
-		// Get Settings.
-		$options = ThemeZee_Blocks_Settings::instance();
-
-		if ( 'valid' !== $options->get( 'license_status' ) ) :
-			?>
-
-			<div class="updated">
-				<p>
-					<?php
-					printf( __( 'Please activate your license key for ThemeZee Blocks in order to receive updates and support. <a href="%s">Activate License</a>', 'themezee-blocks' ),
-						admin_url( 'options-general.php?page=themezee-blocks' )
-					);
-					?>
-				</p>
-			</div>
-
-			<?php
-		endif;
-	}
-
-	/**
-	 * Plugin Updater
-	 *
-	 * @return void
-	 */
-	static function plugin_updater() {
-
-		if ( ! is_admin() ) :
-			return;
-		endif;
-
-		$options = ThemeZee_Blocks_Settings::instance();
-
-		if ( 'valid' === $options->get( 'license_status' ) ) :
-
-			// setup the updater
-			$tzss_updater = new ThemeZee_Blocks_Plugin_Updater( THEMEZEE_BLOCKS_STORE_API_URL, __FILE__, array(
-				'version' => THEMEZEE_BLOCKS_VERSION,
-				'license' => trim( $options->get( 'license_key' ) ),
-				'item_id' => THEMEZEE_BLOCKS_PRODUCT_ID,
-			) );
-
-		endif;
 	}
 }
 
