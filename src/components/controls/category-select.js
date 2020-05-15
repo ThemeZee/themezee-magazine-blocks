d356d08adef9ff221e4b8cc5d379450340f99c70
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-05-15 17:03:04 +0200

    tried out checkbox select

--- src/components/controls/category-select.js
+++ src/components/controls/category-select.js
@@ -1,49 +1,89 @@
 /**
+ * External dependencies
+ */
+const { remove } = lodash;
+
+/**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
 const { Component } = wp.element;
 const { compose } = wp.compose;
 const { withSelect } = wp.data;
-const { SelectControl } = wp.components;
+const { BaseControl, CheckboxControl } = wp.components;
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
 
 class CategorySelect extends Component {
-	/**
-	 * Returns terms in a simplified array.
-	 *
-	 * @param {Array} categoryInput  Array of terms from getEntityRecords.
-	 *
-	 * @return {Array} Array of terms in simplified array.
-	 */
-	createOptions( categoryInput ) {
-		// Return early if categoryList is empty.
-		if ( typeof categoryInput === 'undefined' || categoryInput.length < 1 ) {
-			return [];
+
+	updateSelectedCategories( categoryId, selectedCategoryIds ) {
+		// Initialize array if selectedCategories is undefined.
+		if ( typeof selectedCategoryIds === 'undefined' ) {
+			selectedCategoryIds = [];
 		}
 
-		// Return category options.
-		return categoryInput.map( cat => ( { label: cat.name, value: cat.id, disabled: false } ) );
+		// Check if category is selected or unselected.
+		if ( this.isSelected( categoryId ) ) {
+			// Remove category id.
+			remove( selectedCategoryIds, id => id === categoryId );
+		} else {
+			// Add category id.
+			selectedCategoryIds.push( categoryId );
+		}
+
+		return selectedCategoryIds;
+	}
+
+	isSelected( id, selectedCategoryIds ) {
+
+		// Return early if categoryList is empty.
+		if ( typeof selectedCategoryIds === 'undefined' || selectedCategoryIds.length < 1 ) {
+			return false;
+		}
+
+		return selectedCategoryIds.includes( id );
 	}
 
 	render() {
 		const {
 			categoriesList,
-			selectedCategoryIds,
 			onCategoryChange,
+			selectedCategoryIds,
 		} = this.props;
 
-		const categoryOptions = this.createOptions( categoriesList );
+		// Return early if categoryList is empty.
+		if ( typeof categoriesList === 'undefined' || categoriesList.length < 1 ) {
+			return null;
+		}
 
 		return (
-			onCategoryChange && (
-				<SelectControl
-					multiple
-					label={ __( 'Category', 'themezee-magazine-blocks' ) }
-					value={ selectedCategoryIds }
-					options={ categoryOptions }
-					onChange={ onCategoryChange }
-				/>
-			)
+			<BaseControl
+				label={ __( 'Categories', 'themezee-magazine-blocks' ) }
+				className="tz-category-select"
+			>
+
+				<div className="tz-category-select-container">
+
+					{ categoriesList.map( ( category ) =>
+
+						<CheckboxControl
+							key={ category.id }
+							label={ category.name }
+							checked={ this.isSelected( category.id, selectedCategoryIds ) }
+							onChange={ () => {
+								const selectedCategories = this.updateSelectedCategories( category.id, selectedCategoryIds );
+								return onCategoryChange( selectedCategories );
+							} }
+						/>
+
+					) }
+
+				</div>
+
+			</BaseControl>
 		);
 	}
 }
