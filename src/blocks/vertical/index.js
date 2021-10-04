359f6f6796e866db1092f40950a4f5125e9b2865
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-10-04 19:14:15 +0200

    added block.json for Magazine Vertical block

--- src/blocks/vertical/index.js
+++ src/blocks/vertical/index.js
@@ -1,115 +1,93 @@
 /**
  * WordPress dependencies
  */
-const { __ } = wp.i18n;
-const { createBlock, registerBlockType } = wp.blocks;
+import { createBlock, registerBlockType } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 import './editor.scss';
-import blockAttributes from './attributes';
+import metadata from './block.json';
 import edit from './edit';
 import { IconMagazineVertical } from '../../components/data/icons';
 
 /**
  * Register block
  */
-registerBlockType(
-	'themezee-magazine-blocks/vertical',
-	{
-		title: __( 'Magazine Vertical', 'themezee-magazine-blocks' ),
+registerBlockType( metadata, {
+	icon: IconMagazineVertical,
 
-		description: __( 'Displays your latest posts in a vertical box.', 'themezee-magazine-blocks' ),
-
-		category: 'themezee-magazine-blocks',
-
-		icon: IconMagazineVertical,
-
-		attributes: blockAttributes,
-
-		keywords: [
-			__( 'Posts', 'themezee-magazine-blocks' ),
-			__( 'Box', 'themezee-magazine-blocks' ),
-			__( 'ThemeZee', 'themezee-magazine-blocks' ),
-		],
-
-		supports: {
-			html: false,
-		},
-
-		transforms: {
-			to: [
-				{
-					type: 'block',
-					blocks: [ 'themezee-magazine-blocks/columns' ],
-					transform: ( attributes ) => {
-						return createBlock(
-							'themezee-magazine-blocks/columns',
-							{},
-							[
-								createBlock( 'themezee-magazine-blocks/column', { ...attributes } ),
-								createBlock( 'themezee-magazine-blocks/column', { ...attributes } ),
-							],
-						);
-					},
-				},
-				{
-					type: 'block',
-					blocks: [ 'themezee-magazine-blocks/grid' ],
-					transform: ( attributes ) => {
-						return createBlock( 'themezee-magazine-blocks/grid', { ...attributes } );
-					},
-				},
-				{
-					type: 'block',
-					blocks: [ 'themezee-magazine-blocks/horizontal' ],
-					transform: ( attributes ) => {
-						return createBlock( 'themezee-magazine-blocks/horizontal', { ...attributes } );
-					},
-				},
-				{
-					type: 'block',
-					blocks: [ 'themezee-magazine-blocks/list' ],
-					transform: ( attributes ) => {
-						return createBlock( 'themezee-magazine-blocks/list', { ...attributes } );
-					},
-				},
-			],
-		},
-
-		edit,
-
-		// Block is rendered server-side.
-		save: () => {},
-
-		deprecated: [
+	transforms: {
+		to: [
 			{
-				attributes: {
-					...blockAttributes,
-					categories: {
-						type: 'string',
-					},
+				type: 'block',
+				blocks: [ 'themezee-magazine-blocks/columns' ],
+				transform: ( attributes ) => {
+					return createBlock(
+						'themezee-magazine-blocks/columns',
+						{},
+						[
+							createBlock( 'themezee-magazine-blocks/column', { ...attributes } ),
+							createBlock( 'themezee-magazine-blocks/column', { ...attributes } ),
+						],
+					);
 				},
-
-				supports: {
-					html: false,
+			},
+			{
+				type: 'block',
+				blocks: [ 'themezee-magazine-blocks/grid' ],
+				transform: ( attributes ) => {
+					return createBlock( 'themezee-magazine-blocks/grid', { ...attributes } );
 				},
-
-				migrate( oldAttributes ) {
-					return {
-						...oldAttributes,
-						categories: [ oldAttributes.categories ].map( ( cat ) => parseInt( cat ) ),
-					};
+			},
+			{
+				type: 'block',
+				blocks: [ 'themezee-magazine-blocks/horizontal' ],
+				transform: ( attributes ) => {
+					return createBlock( 'themezee-magazine-blocks/horizontal', { ...attributes } );
 				},
-
-				isEligible: ( { categories } ) => {
-					return categories && 'string' === typeof categories;
+			},
+			{
+				type: 'block',
+				blocks: [ 'themezee-magazine-blocks/list' ],
+				transform: ( attributes ) => {
+					return createBlock( 'themezee-magazine-blocks/list', { ...attributes } );
 				},
-
-				save: () => {},
 			},
 		],
 	},
-);
+
+	edit,
+
+	// Block is rendered server-side.
+	save: () => {},
+
+	deprecated: [
+		{
+			attributes: {
+				...metadata.attributes,
+				categories: {
+					type: 'string',
+				},
+			},
+
+			supports: {
+				html: false,
+			},
+
+			migrate( oldAttributes ) {
+				return {
+					...oldAttributes,
+					categories: [ oldAttributes.categories ].map( ( cat ) => parseInt( cat ) ),
+				};
+			},
+
+			isEligible: ( { categories } ) => {
+				return categories && 'string' === typeof categories;
+			},
+
+			save: () => {},
+		},
+	],
+} );
