8602aa4867d5da3fa9f8555e74976c57231108b8
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-10-04 19:22:37 +0200

    added deprecated and transform files for Magazine Vertical block

--- src/blocks/vertical/index.js
+++ src/blocks/vertical/index.js
@@ -1,93 +1,28 @@
 /**
  * WordPress dependencies
  */
-import { createBlock, registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 import './editor.scss';
+import { IconMagazineVertical as icon } from '../../components/data/icons';
 import metadata from './block.json';
 import edit from './edit';
-import { IconMagazineVertical } from '../../components/data/icons';
+import deprecated from './deprecated';
+import transforms from './transforms';
 
 /**
  * Register block
  */
 registerBlockType( metadata, {
-	icon: IconMagazineVertical,
-
-	transforms: {
-		to: [
-			{
-				type: 'block',
-				blocks: [ 'themezee-magazine-blocks/columns' ],
-				transform: ( attributes ) => {
-					return createBlock(
-						'themezee-magazine-blocks/columns',
-						{},
-						[
-							createBlock( 'themezee-magazine-blocks/column', { ...attributes } ),
-							createBlock( 'themezee-magazine-blocks/column', { ...attributes } ),
-						],
-					);
-				},
-			},
-			{
-				type: 'block',
-				blocks: [ 'themezee-magazine-blocks/grid' ],
-				transform: ( attributes ) => {
-					return createBlock( 'themezee-magazine-blocks/grid', { ...attributes } );
-				},
-			},
-			{
-				type: 'block',
-				blocks: [ 'themezee-magazine-blocks/horizontal' ],
-				transform: ( attributes ) => {
-					return createBlock( 'themezee-magazine-blocks/horizontal', { ...attributes } );
-				},
-			},
-			{
-				type: 'block',
-				blocks: [ 'themezee-magazine-blocks/list' ],
-				transform: ( attributes ) => {
-					return createBlock( 'themezee-magazine-blocks/list', { ...attributes } );
-				},
-			},
-		],
-	},
-
+	icon,
+	deprecated,
+	transforms,
 	edit,
 
 	// Block is rendered server-side.
 	save: () => {},
-
-	deprecated: [
-		{
-			attributes: {
-				...metadata.attributes,
-				categories: {
-					type: 'string',
-				},
-			},
-
-			supports: {
-				html: false,
-			},
-
-			migrate( oldAttributes ) {
-				return {
-					...oldAttributes,
-					categories: [ oldAttributes.categories ].map( ( cat ) => parseInt( cat ) ),
-				};
-			},
-
-			isEligible: ( { categories } ) => {
-				return categories && 'string' === typeof categories;
-			},
-
-			save: () => {},
-		},
-	],
 } );
